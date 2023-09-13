4c3731d9b8b2aebec34fff6590a6a689910ce7d0
Jon Cram <webignition@gmail.com>
2023-09-13 18:01:44 +0100

    Handle /user/frontend/token/create

---
+++ tests/Functional/Client/AbstractClientTestCase.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Tests\Functional\Client;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\HttpFactory;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ApiClient\Client;
+use SmartAssert\ApiClient\Tests\Functional\DataProvider\CommonNonSuccessResponseDataProviderTrait;
+use SmartAssert\ApiClient\Tests\Functional\DataProvider\InvalidJsonResponseExceptionDataProviderTrait;
+use SmartAssert\ApiClient\Tests\Functional\DataProvider\NetworkErrorExceptionDataProviderTrait;
+use SmartAssert\ServiceClient\Client as ServiceClient;
+use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
+use SmartAssert\ServiceClient\ExceptionFactory\CurlExceptionFactory;
+use SmartAssert\ServiceClient\ResponseFactory\ResponseFactory;
+use webignition\HttpHistoryContainer\Container as HttpHistoryContainer;
+
+abstract class AbstractClientTestCase extends TestCase
+{
+    use CommonNonSuccessResponseDataProviderTrait;
+    use InvalidJsonResponseExceptionDataProviderTrait;
+    use NetworkErrorExceptionDataProviderTrait;
+
+    protected MockHandler $mockHandler;
+    protected Client $client;
+    private HttpHistoryContainer $httpHistoryContainer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockHandler = new MockHandler();
+
+        $httpFactory = new HttpFactory();
+
+        $handlerStack = HandlerStack::create($this->mockHandler);
+
+        $this->httpHistoryContainer = new HttpHistoryContainer();
+        $handlerStack->push(Middleware::history($this->httpHistoryContainer));
+
+        $this->client = new Client(
+            'https://users.example.com',
+            new ServiceClient(
+                $httpFactory,
+                $httpFactory,
+                new HttpClient(['handler' => $handlerStack]),
+                ResponseFactory::createFactory(),
+                new CurlExceptionFactory(),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider networkErrorExceptionDataProvider
+     * @dataProvider invalidJsonResponseExceptionDataProvider
+     *
+     * @param class-string<\Throwable> $expectedExceptionClass
+     */
+    public function testClientActionThrowsException(
+        ResponseInterface|ClientExceptionInterface $httpFixture,
+        string $expectedExceptionClass,
+    ): void {
+        $this->mockHandler->append($httpFixture);
+
+        $this->expectException($expectedExceptionClass);
+
+        ($this->createClientActionCallable())();
+    }
+
+    /**
+     * @dataProvider commonNonSuccessResponseDataProvider
+     */
+    public function testClientActionThrowsNonSuccessResponseException(ResponseInterface $httpFixture): void
+    {
+        $this->mockHandler->append($httpFixture);
+
+        try {
+            ($this->createClientActionCallable())();
+
+            self::fail(NonSuccessResponseException::class . ' not thrown');
+        } catch (NonSuccessResponseException $e) {
+            self::assertSame($httpFixture, $e->response);
+        }
+    }
+
+    protected function getLastRequest(): RequestInterface
+    {
+        $request = $this->httpHistoryContainer->getTransactions()->getRequests()->getLast();
+        \assert($request instanceof RequestInterface);
+
+        return $request;
+    }
+
+    abstract protected function createClientActionCallable(): callable;
+}
---
+++ tests/Functional/DataProvider/NetworkErrorExceptionDataProviderTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Tests\Functional\DataProvider;
+
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Client\ClientExceptionInterface;
+
+trait NetworkErrorExceptionDataProviderTrait
+{
+    /**
+     * @return array<mixed>
+     */
+    public static function networkErrorExceptionDataProvider(): array
+    {
+        return [
+            'network error' => [
+                'httpFixture' => new ConnectException('Exception message', new Request('GET', '/')),
+                'expectedExceptionClass' => ClientExceptionInterface::class,
+            ],
+        ];
+    }
+}
