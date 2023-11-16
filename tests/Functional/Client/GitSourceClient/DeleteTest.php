5e2fbfce208a4164536ba7f2afa1a33eb72c2f01
Jon Cram <webignition@gmail.com>
2023-11-16 11:04:01 +0000

    Handle DELETE /git-source/{sourceId}

---
+++ tests/Functional/Client/GitSourceClient/DeleteTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Tests\Functional\Client\GitSourceClient;
+
+use SmartAssert\ApiClient\Tests\Functional\Client\ExpectedRequestProperties;
+use SmartAssert\ApiClient\Tests\Functional\Client\RequestPropertiesTestTrait;
+use SmartAssert\ApiClient\Tests\Functional\DataProvider\InvalidJsonResponseExceptionDataProviderTrait;
+use SmartAssert\ApiClient\Tests\Functional\DataProvider\NetworkErrorExceptionDataProviderTrait;
+
+class DeleteTest extends AbstractSourceClientTestCase
+{
+    use InvalidJsonResponseExceptionDataProviderTrait;
+    use NetworkErrorExceptionDataProviderTrait;
+    use RequestPropertiesTestTrait;
+
+    private const DELETED_AT = 123456;
+
+    public static function clientActionThrowsExceptionDataProvider(): array
+    {
+        return array_merge(
+            self::networkErrorExceptionDataProvider(),
+            self::invalidJsonResponseExceptionDataProvider(),
+        );
+    }
+
+    protected function createClientActionCallable(): callable
+    {
+        return function () {
+            $this->client->delete(self::API_KEY, self::ID);
+        };
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    protected function getResponsePayload(): array
+    {
+        return [
+            'git_source' => [
+                'id' => self::ID,
+                'label' => self::LABEL,
+                'host_url' => self::HOST_URL,
+                'path' => self::PATH,
+                'has_credentials' => self::HAS_CREDENTIALS,
+                'deleted_at' => self::DELETED_AT,
+            ],
+        ];
+    }
+
+    protected function getExpectedRequestProperties(): ExpectedRequestProperties
+    {
+        return new ExpectedRequestProperties('DELETE', '/git-source/' . self::ID);
+    }
+}
