f5794cab74cf633beda29c979a065261eaeb814f
Jon Cram <webignition@gmail.com>
2023-11-16 09:49:35 +0000

    Handle GET /git-source/{sourceId}

--- tests/Integration/GitSource/CreateTest.php
+++ tests/Integration/GitSource/CreateTest.php
@@ -8,6 +8,8 @@ use SmartAssert\ServiceClient\Exception\UnauthorizedException;
 
 class CreateTest extends AbstractGitSourceTestCase
 {
+    use CreateDataProviderTrait;
+
     public function testCreateUnauthorized(): void
     {
         self::expectException(UnauthorizedException::class);
@@ -41,28 +43,5 @@ class CreateTest extends AbstractGitSourceTestCase
         self::assertSame($hostUrl, $source->hostUrl);
         self::assertSame($path, $source->path);
         self::assertSame($expectedHasCredentials, $source->hasCredentials);
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public static function createDataProvider(): array
-    {
-        return [
-            'without credentials' => [
-                'label' => md5((string) rand()),
-                'hostUrl' => md5((string) rand()),
-                'path' => md5((string) rand()),
-                'credentials' => null,
-                'expectedHasCredentials' => false,
-            ],
-            'with credentials' => [
-                'label' => md5((string) rand()),
-                'hostUrl' => md5((string) rand()),
-                'path' => md5((string) rand()),
-                'credentials' => md5((string) rand()),
-                'expectedHasCredentials' => true,
-            ],
-        ];
     }
 }
---
+++ tests/Integration/GitSource/GetTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Tests\Integration\GitSource;
+
+use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
+use SmartAssert\ServiceClient\Exception\UnauthorizedException;
+use Symfony\Component\Uid\Ulid;
+
+class GetTest extends AbstractGitSourceTestCase
+{
+    use CreateDataProviderTrait;
+
+    public function testGetUnauthorized(): void
+    {
+        self::expectException(UnauthorizedException::class);
+        $id = (string) new Ulid();
+        \assert('' !== $id);
+
+        self::$gitSourceClient->get(md5((string) rand()), $id);
+    }
+
+    public function testGetNotFound(): void
+    {
+        $refreshableToken = self::$usersClient->createToken(self::USER1_EMAIL, self::USER1_PASSWORD);
+        $apiKey = self::$usersClient->getApiKey($refreshableToken->token);
+
+        $id = (string) new Ulid();
+        \assert('' !== $id);
+
+        self::expectException(NonSuccessResponseException::class);
+        self::expectExceptionCode(404);
+
+        self::$gitSourceClient->get($apiKey->key, $id);
+    }
+
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testGetSuccess(
+        string $label,
+        string $hostUrl,
+        string $path,
+        ?string $credentials,
+        bool $expectedHasCredentials
+    ): void {
+        $refreshableToken = self::$usersClient->createToken(self::USER1_EMAIL, self::USER1_PASSWORD);
+        $apiKey = self::$usersClient->getApiKey($refreshableToken->token);
+
+        $createdSource = self::$gitSourceClient->create($apiKey->key, $label, $hostUrl, $path, $credentials);
+        $retrievedSource = self::$gitSourceClient->get($apiKey->key, $createdSource->id);
+
+        self::assertSame($createdSource->id, $retrievedSource->id);
+        self::assertSame($label, $retrievedSource->label);
+        self::assertSame($hostUrl, $retrievedSource->hostUrl);
+        self::assertSame($path, $retrievedSource->path);
+        self::assertSame($expectedHasCredentials, $retrievedSource->hasCredentials);
+    }
+}
