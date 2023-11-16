a59e32840fc871f02d53dadb60a718ef83051a1a
Jon Cram <webignition@gmail.com>
2023-11-16 11:09:10 +0000

    Handle PUT /git-source/{sourceId}

--- src/GitSourceClient.php
+++ src/GitSourceClient.php
@@ -85,14 +85,36 @@ readonly class GitSourceClient
      */
     public function get(string $apiKey, string $id): GitSource
     {
-        $response = $this->serviceClient->sendRequest(
-            (new Request('GET', $this->urlGenerator->generate('git-source', ['sourceId' => $id])))
-                ->withAuthentication(
-                    new BearerAuthentication($apiKey)
-                )
-        );
+        return $this->handleRequest($apiKey, 'GET', $id);
+    }
 
-        return $this->handleGitSourceResponse($response);
+    /**
+     * @param non-empty-string  $apiKey
+     * @param non-empty-string  $id
+     * @param non-empty-string  $label
+     * @param non-empty-string  $hostUrl
+     * @param non-empty-string  $path
+     * @param ?non-empty-string $credentials
+     *
+     * @throws ClientExceptionInterface
+     * @throws NetworkExceptionInterface
+     * @throws CurlExceptionInterface
+     * @throws RequestExceptionInterface
+     * @throws NonSuccessResponseException
+     * @throws InvalidResponseTypeException
+     * @throws InvalidResponseDataException
+     * @throws InvalidModelDataException
+     * @throws UnauthorizedException
+     */
+    public function update(
+        string $apiKey,
+        string $id,
+        string $label,
+        string $hostUrl,
+        string $path,
+        ?string $credentials,
+    ): GitSource {
+        return $this->handleRequest($apiKey, 'PUT', $id, $label, $hostUrl, $path, $credentials);
     }
 
     /**
@@ -111,12 +133,63 @@ readonly class GitSourceClient
      */
     public function delete(string $apiKey, string $id): GitSource
     {
-        $response = $this->serviceClient->sendRequest(
-            (new Request('DELETE', $this->urlGenerator->generate('git-source', ['sourceId' => $id])))
-                ->withAuthentication(
-                    new BearerAuthentication($apiKey)
-                )
-        );
+        return $this->handleRequest($apiKey, 'DELETE', $id);
+    }
+
+    /**
+     * @param non-empty-string  $apiKey
+     * @param non-empty-string  $method
+     * @param non-empty-string  $id
+     * @param ?non-empty-string $label
+     * @param ?non-empty-string $hostUrl
+     * @param ?non-empty-string $path
+     * @param ?non-empty-string $credentials
+     *
+     * @throws ClientExceptionInterface
+     * @throws CurlExceptionInterface
+     * @throws InvalidModelDataException
+     * @throws InvalidResponseDataException
+     * @throws InvalidResponseTypeException
+     * @throws NetworkExceptionInterface
+     * @throws NonSuccessResponseException
+     * @throws RequestExceptionInterface
+     * @throws UnauthorizedException
+     */
+    private function handleRequest(
+        string $apiKey,
+        string $method,
+        string $id,
+        ?string $label = null,
+        ?string $hostUrl = null,
+        ?string $path = null,
+        ?string $credentials = null,
+    ): GitSource {
+        $request = new Request($method, $this->urlGenerator->generate('git-source', ['sourceId' => $id]));
+        $request = $request->withAuthentication(new BearerAuthentication($apiKey));
+
+        $payload = [];
+
+        if (null !== $label) {
+            $payload['label'] = $label;
+        }
+
+        if (null !== $hostUrl) {
+            $payload['host-url'] = $hostUrl;
+        }
+
+        if (null !== $path) {
+            $payload['path'] = $path;
+        }
+
+        if (null !== $credentials) {
+            $payload['credentials'] = $credentials;
+        }
+
+        if ([] !== $payload) {
+            $request = $request->withPayload(new UrlEncodedPayload($payload));
+        }
+
+        $response = $this->serviceClient->sendRequest($request);
 
         return $this->handleGitSourceResponse($response);
     }
