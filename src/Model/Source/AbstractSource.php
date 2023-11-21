b1b2545b77322d513c2df4cd23f4324a1df183be
Jon Cram <webignition@gmail.com>
2023-11-21 16:13:45 +0000

    Create SourceInterface (#115)

--- src/Model/Source/AbstractSource.php
+++ src/Model/Source/AbstractSource.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SmartAssert\ApiClient\Model\Source;
 
-abstract readonly class AbstractSource
+abstract readonly class AbstractSource implements SourceInterface
 {
     /**
      * @param non-empty-string $id
@@ -16,5 +16,20 @@ abstract readonly class AbstractSource
         public string $label,
         public ?int $deletedAt,
     ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getDeletedAt(): ?int
+    {
+        return $this->deletedAt;
     }
 }
