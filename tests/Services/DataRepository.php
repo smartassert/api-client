aec9c3c088cd3482285fac59a093d48b79cc44db
Jon Cram <webignition@gmail.com>
2024-01-22 10:24:17 +0000

    Create exception for user default api key not found

--- tests/Services/DataRepository.php
+++ tests/Services/DataRepository.php
@@ -11,6 +11,7 @@ class DataRepository
     public function __construct(
         private readonly string $databaseDsn,
     ) {
+        self::$connection = null;
     }
 
     /**
@@ -30,5 +31,10 @@ class DataRepository
         }
 
         return self::$connection;
+    }
+
+    public function query(string $query): void
+    {
+        $this->getConnection()->query($query);
     }
 }
