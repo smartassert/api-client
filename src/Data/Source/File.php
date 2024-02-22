f8994964de3710b9f8519540980560e436417787
Jon Cram <webignition@gmail.com>
2024-02-22 14:15:13 +0000

    Use sources 18.0

---
+++ src/Data/Source/File.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Data\Source;
+
+readonly class File
+{
+    /**
+     * @param non-empty-string $path
+     */
+    public function __construct(
+        public string $path,
+        public int $size,
+    ) {
+    }
+}
