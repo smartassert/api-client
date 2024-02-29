f84c15db01fbca691a89261a9e6420da1744a7b2
Jon Cram <webignition@gmail.com>
2024-02-29 11:34:13 +0000

    Handle 403 when updating git source to which user does not have access

--- tests/Integration/GitSource/UpdateTest.php
+++ tests/Integration/GitSource/UpdateTest.php
@@ -6,6 +6,8 @@ namespace SmartAssert\ApiClient\Tests\Integration\GitSource;
 
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Psr7\HttpFactory;
+use SmartAssert\ApiClient\Data\Source\GitSource;
+use SmartAssert\ApiClient\Data\User\ApiKey;
 use SmartAssert\ApiClient\Exception\ClientException;
 use SmartAssert\ApiClient\Exception\Error\ErrorException;
 use SmartAssert\ApiClient\Exception\Error\Factory as ExceptionFactory;
@@ -212,5 +214,34 @@ class UpdateTest extends AbstractIntegrationTestCase
         self::assertSame($createdSource->hostUrl, $updatedSource->hostUrl);
         self::assertSame($createdSource->path, $updatedSource->path);
         self::assertSame($expectedHasCredentials, $updatedSource->hasCredentials);
+    }
+
+    public function testUpdateForbidden(): void
+    {
+        $this->doForbiddenActionTest(
+            function (ApiKey $apiKey) {
+                return self::$gitSourceClient->create(
+                    $apiKey->key,
+                    md5((string) rand()),
+                    md5((string) rand()),
+                    md5((string) rand()),
+                    null
+                );
+            },
+            function (ApiKey $apiKey, ?object $source) {
+                if (!$source instanceof GitSource) {
+                    return;
+                }
+
+                self::$gitSourceClient->update(
+                    $apiKey->key,
+                    $source->id,
+                    md5((string) rand()),
+                    md5((string) rand()),
+                    md5((string) rand()),
+                    null
+                );
+            },
+        );
     }
 }
