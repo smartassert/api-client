d6b3b490621ed9bce302d023885d8c65d326b867
Jon Cram <webignition@gmail.com>
2024-03-06 16:54:30 +0000

    Handle 403 when getting suite to which user does not have access (to related source)

--- tests/Integration/Suite/GetTest.php
+++ tests/Integration/Suite/GetTest.php
@@ -7,6 +7,7 @@ namespace SmartAssert\ApiClient\Tests\Integration\Suite;
 use SmartAssert\ApiClient\Exception\ClientException;
 use SmartAssert\ApiClient\Exception\ForbiddenException;
 use SmartAssert\ApiClient\Exception\UnauthorizedException;
+use SmartAssert\ApiClient\Tests\Services\SourcesRepository;
 use Symfony\Component\Uid\Ulid;
 
 class GetTest extends AbstractSuiteTestCase
@@ -59,6 +60,37 @@ class GetTest extends AbstractSuiteTestCase
 
         $source = self::$fileSourceClient->create($user2ApiKey->key, md5((string) rand()));
         $suite = self::$suiteClient->create($user2ApiKey->key, $source->id, md5((string) rand()), []);
+
+        $exception = null;
+
+        try {
+            self::$suiteClient->get($user1ApiKey->key, $suite->id);
+        } catch (ClientException $exception) {
+        }
+
+        self::assertInstanceOf(ClientException::class, $exception);
+        self::assertInstanceOf(ForbiddenException::class, $exception->getInnerException());
+    }
+
+    public function testGetSourceForbidden(): void
+    {
+        $user1RefreshableToken = self::$usersClient->createToken(self::USER1_EMAIL, self::USER1_PASSWORD);
+        $user1ApiKey = self::$usersClient->getApiKey($user1RefreshableToken->token);
+
+        $user2RefreshableToken = self::$usersClient->createToken(self::USER2_EMAIL, self::USER2_PASSWORD);
+        $user2ApiKey = self::$usersClient->getApiKey($user2RefreshableToken->token);
+
+        $user1Source = self::$fileSourceClient->create($user1ApiKey->key, md5((string) rand()));
+        $user2Source = self::$fileSourceClient->create($user2ApiKey->key, md5((string) rand()));
+
+        $suite = self::$suiteClient->create($user1ApiKey->key, $user1Source->id, md5((string) rand()), []);
+
+        $sourcesRepository = new SourcesRepository();
+        $sourcesRepository->getConnection()->query(sprintf(
+            "UPDATE public.suite SET source_id='%s' WHERE id='%s';",
+            $user2Source->id,
+            $suite->id
+        ));
 
         $exception = null;
 
