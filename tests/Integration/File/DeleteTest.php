91c001bf65e0327413da331a058d96bb1c9cf976
Jon Cram <webignition@gmail.com>
2024-03-06 17:26:15 +0000

    Handle 403 when deleting file source file to which user does not have access (to related source)

--- tests/Integration/File/DeleteTest.php
+++ tests/Integration/File/DeleteTest.php
@@ -6,6 +6,7 @@ namespace SmartAssert\ApiClient\Tests\Integration\File;
 
 use SmartAssert\ApiClient\Exception\ClientException;
 use SmartAssert\ApiClient\Exception\File\NotFoundException as FileNotFoundException;
+use SmartAssert\ApiClient\Exception\ForbiddenException;
 
 class DeleteTest extends AbstractFileTestCase
 {
@@ -39,5 +40,29 @@ class DeleteTest extends AbstractFileTestCase
 
         self::expectNotToPerformAssertions();
         self::$fileClient->delete($apiKey->key, $source->getId(), '');
+    }
+
+    public function testDeleteSourceForbidden(): void
+    {
+        $user1RefreshableToken = self::$usersClient->createToken(self::USER1_EMAIL, self::USER1_PASSWORD);
+        $user1ApiKey = self::$usersClient->getApiKey($user1RefreshableToken->token);
+
+        $user2RefreshableToken = self::$usersClient->createToken(self::USER2_EMAIL, self::USER2_PASSWORD);
+        $user2ApiKey = self::$usersClient->getApiKey($user2RefreshableToken->token);
+
+        $source = self::$fileSourceClient->create($user2ApiKey->key, md5((string) rand()));
+
+        $filename = md5((string) rand()) . '.yaml';
+        self::$fileClient->create($user2ApiKey->key, $source->id, $filename, md5((string) rand()));
+
+        $exception = null;
+
+        try {
+            self::$fileClient->delete($user1ApiKey->key, $source->getId(), $filename);
+        } catch (ClientException $exception) {
+        }
+
+        self::assertInstanceOf(ClientException::class, $exception);
+        self::assertInstanceOf(ForbiddenException::class, $exception->getInnerException());
     }
 }
