0c781a7d2661d055842618f72c3f49bed955e79f
Jon Cram <webignition@gmail.com>
2024-03-06 17:05:20 +0000

    Handle 403 when creating file source file to which user does not have access (to related source)

--- tests/Integration/File/CreateTest.php
+++ tests/Integration/File/CreateTest.php
@@ -6,6 +6,7 @@ namespace SmartAssert\ApiClient\Tests\Integration\File;
 
 use SmartAssert\ApiClient\Exception\ClientException;
 use SmartAssert\ApiClient\Exception\Error\ErrorException;
+use SmartAssert\ApiClient\Exception\ForbiddenException;
 use SmartAssert\ApiClient\Exception\UnauthorizedException;
 use SmartAssert\ServiceRequest\Error\BadRequestError;
 use SmartAssert\ServiceRequest\Error\BadRequestErrorInterface;
@@ -101,5 +102,31 @@ class CreateTest extends AbstractFileTestCase
         $error = $errorException->getError();
         self::assertInstanceOf(DuplicateObjectErrorInterface::class, $error);
         self::assertEquals(new DuplicateObjectError(new Parameter('filename', $filename)), $error);
+    }
+
+    public function testCreateSourceForbidden(): void
+    {
+        $user1RefreshableToken = self::$usersClient->createToken(self::USER1_EMAIL, self::USER1_PASSWORD);
+        $user1ApiKey = self::$usersClient->getApiKey($user1RefreshableToken->token);
+
+        $user2RefreshableToken = self::$usersClient->createToken(self::USER2_EMAIL, self::USER2_PASSWORD);
+        $user2ApiKey = self::$usersClient->getApiKey($user2RefreshableToken->token);
+
+        $source = self::$fileSourceClient->create($user2ApiKey->key, md5((string) rand()));
+
+        $exception = null;
+
+        try {
+            self::$fileClient->create(
+                $user1ApiKey->key,
+                $source->id,
+                md5((string) rand()) . '.yaml',
+                md5((string) rand())
+            );
+        } catch (ClientException $exception) {
+        }
+
+        self::assertInstanceOf(ClientException::class, $exception);
+        self::assertInstanceOf(ForbiddenException::class, $exception->getInnerException());
     }
 }
