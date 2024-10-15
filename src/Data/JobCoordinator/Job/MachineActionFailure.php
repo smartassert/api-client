804cadf2723faf4b386d705730238db1a36568d8
Jon Cram <webignition@gmail.com>
2024-10-15 10:15:15 +0100

    Add machine action failure (#287)

---
+++ src/Data/JobCoordinator/Job/MachineActionFailure.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Data\JobCoordinator\Job;
+
+readonly class MachineActionFailure
+{
+    /**
+     * @param non-empty-string            $action
+     * @param non-empty-string            $type
+     * @param null|non-empty-array<mixed> $context
+     */
+    public function __construct(
+        public string $action,
+        public string $type,
+        public ?array $context,
+    ) {
+    }
+}
--- tests/Functional/Client/JobCoordinatorClient/GetTest.php
+++ tests/Functional/Client/JobCoordinatorClient/GetTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace SmartAssert\ApiClient\Tests\Functional\Client\JobCoordinatorClient;
 
 use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ApiClient\Data\JobCoordinator\Job\Job;
+use SmartAssert\ApiClient\Data\JobCoordinator\Job\MachineActionFailure;
 use SmartAssert\ApiClient\Tests\Functional\Client\ClientActionThrowsIncompleteDataExceptionTestTrait;
 use SmartAssert\ApiClient\Tests\Functional\Client\ExpectedRequestProperties;
 use SmartAssert\ApiClient\Tests\Functional\Client\RequestAuthenticationTestTrait;
@@ -43,10 +46,119 @@ class GetTest extends AbstractJobCoordinatorClientTestCase
         ];
     }
 
+    /**
+     * @param null|array<mixed> $responseMachineActionFailureData
+     */
+    #[DataProvider('fooDataProvider')]
+    public function testFoo(
+        ?array $responseMachineActionFailureData,
+        ?MachineActionFailure $expectedMachineActionFailure,
+    ): void {
+        $this->getMockHandler()->append(new Response(
+            200,
+            ['content-type' => 'application/json'],
+            (string) json_encode([
+                'id' => self::ID,
+                'suite_id' => self::SUITE_ID,
+                'maximum_duration_in_seconds' => self::MAXIMUM_DURATION_IN_SECONDS,
+                'preparation' => [
+                    'state' => 'requesting',
+                ],
+                'results_job' => [],
+                'serialized_suite' => [],
+                'machine' => [
+                    'state_category' => 'failed',
+                    'ip_address' => null,
+                    'action_failure' => $responseMachineActionFailureData,
+                ],
+                'worker_job' => [
+                    'state' => 'pending',
+                ],
+                'service_requests' => [],
+            ])
+        ));
+
+        $job = ($this->createClientActionCallable())();
+
+        self::assertEquals($expectedMachineActionFailure, $job->machine->actionFailure);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function fooDataProvider(): array
+    {
+        return [
+            'without action failure' => [
+                'responseMachineActionFailureData' => null,
+                'expectedMachineActionFailure' => null,
+            ],
+            'with invalid action failure, empty action' => [
+                'responseMachineActionFailureData' => [
+                    'action' => '',
+                    'type' => md5((string) rand()),
+                    'context' => null,
+                ],
+                'expectedMachineActionFailure' => null,
+            ],
+            'with invalid action failure, empty type' => [
+                'responseMachineActionFailureData' => [
+                    'action' => md5((string) rand()),
+                    'type' => '',
+                    'context' => null,
+                ],
+                'expectedMachineActionFailure' => null,
+            ],
+            'with invalid action failure, non-array context' => [
+                'responseMachineActionFailureData' => [
+                    'action' => 'non-empty action',
+                    'type' => 'non-empty type',
+                    'context' => md5((string) rand()),
+                ],
+                'expectedMachineActionFailure' => new MachineActionFailure(
+                    'non-empty action',
+                    'non-empty type',
+                    null
+                ),
+            ],
+            'with action failure, with null context' => [
+                'responseMachineActionFailureData' => [
+                    'action' => 'get',
+                    'type' => 'get error type',
+                    'context' => null,
+                ],
+                'expectedMachineActionFailure' => new MachineActionFailure(
+                    'get',
+                    'get error type',
+                    null
+                ),
+            ],
+            'with action failure, with context' => [
+                'responseMachineActionFailureData' => [
+                    'action' => 'delete',
+                    'type' => 'delete error type',
+                    'context' => [
+                        'provider' => 'provider name',
+                    ],
+                ],
+                'expectedMachineActionFailure' => new MachineActionFailure(
+                    'delete',
+                    'delete error type',
+                    [
+                        'provider' => 'provider name',
+                    ],
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @return callable(): Job
+     */
     protected function createClientActionCallable(): callable
     {
-        return function () {
-            $this->client->get(self::API_KEY, self::ID);
+        return function (): Job {
+            return $this->client->get(self::API_KEY, self::ID);
         };
     }
 
