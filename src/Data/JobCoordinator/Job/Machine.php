1b516522c2163feff258df84f3b25fd09bc8dacb
Jon Cram <webignition@gmail.com>
2026-02-20 17:46:49 +0000

    Job::machine is nullable; Machine::stateCategory is not (#334)

--- src/Data/JobCoordinator/Job/Machine.php
+++ src/Data/JobCoordinator/Job/Machine.php
@@ -7,11 +7,11 @@ namespace SmartAssert\ApiClient\Data\JobCoordinator\Job;
 readonly class Machine
 {
     /**
-     * @param ?non-empty-string $stateCategory
+     * @param non-empty-string  $stateCategory
      * @param ?non-empty-string $ipAddress
      */
     public function __construct(
-        public ?string $stateCategory,
+        public string $stateCategory,
         public ?string $ipAddress,
         public ?MachineActionFailure $actionFailure,
     ) {}
