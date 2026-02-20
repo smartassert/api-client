30d11b826b9b30e81a69b32fee43ee6671fdc225
Jon Cram <webignition@gmail.com>
2026-02-20 17:07:13 +0000

    Create model

---
+++ src/Data/JobCoordinator/Job/MetaState.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Data\JobCoordinator\Job;
+
+readonly class MetaState
+{
+    public function __construct(
+        public bool $ended,
+        public bool $succeeded,
+    ) {}
+
+}
