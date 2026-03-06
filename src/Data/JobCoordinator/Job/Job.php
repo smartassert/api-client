8e72602918f2bdbe0a5a4d644d9391f360512d50
Jon Cram <webignition@gmail.com>
2026-03-06 14:50:09 +0000

    Add explicit job component getters

--- src/Data/JobCoordinator/Job/Job.php
+++ src/Data/JobCoordinator/Job/Job.php
@@ -18,4 +18,32 @@ readonly class Job implements HasMetastateInterface
         public Components $components,
         public array $serviceRequests,
     ) {}
+
+    public function getResultsJob(): ?ResultsJob
+    {
+        $component = $this->components->get('results-job');
+
+        return $component instanceof ResultsJob ? $component : null;
+    }
+
+    public function getSerializedSuite(): ?SerializedSuite
+    {
+        $component = $this->components->get('serialized-suite');
+
+        return $component instanceof SerializedSuite ? $component : null;
+    }
+
+    public function getMachine(): ?Machine
+    {
+        $component = $this->components->get('machine');
+
+        return $component instanceof Machine ? $component : null;
+    }
+
+    public function getWorkerJob(): ?WorkerJob
+    {
+        $component = $this->components->get('worker-job');
+
+        return $component instanceof WorkerJob ? $component : null;
+    }
 }
