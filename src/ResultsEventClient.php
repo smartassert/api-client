b345cc2652e23c9ce2d3a15c6b6003bd355fffd3
Jon Cram <webignition@gmail.com>
2026-06-09 15:03:02 +0100

    Create client

---
+++ src/ResultsEventClient.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient;
+
+use SmartAssert\ApiClient\Data\Results\Event;
+use SmartAssert\ApiClient\Exception\ClientException;
+use SmartAssert\ApiClient\Exception\IncompleteDataException;
+use SmartAssert\ApiClient\Factory\Results\EventFactory;
+use SmartAssert\ApiClient\Request\Header\ApiKeyAuthorizationHeader;
+use SmartAssert\ApiClient\Request\RequestSpecification;
+use SmartAssert\ApiClient\Request\RouteRequirements;
+use SmartAssert\ApiClient\ServiceClient\HttpHandler;
+
+readonly class ResultsEventClient
+{
+    public function __construct(
+        private EventFactory $eventFactory,
+        private HttpHandler $httpHandler,
+    ) {}
+
+    /**
+     * @param non-empty-string $apiKey
+     *
+     * @return Event[]
+     *
+     * @throws ClientException
+     */
+    public function list(string $apiKey, string $label, ?string $reference, ?string $type): array
+    {
+        $requestSpecification = new RequestSpecification(
+            'GET',
+            new RouteRequirements(
+                'results-event-list',
+                [
+                    'label' => $label,
+                    'reference' => $reference,
+                    'type' => $type,
+                ]
+            ),
+            new ApiKeyAuthorizationHeader($apiKey),
+        );
+
+        $data = $this->httpHandler->getJson($requestSpecification);
+
+        $events = [];
+        foreach ($data as $dataIndex => $eventData) {
+            if (!is_array($eventData)) {
+                continue;
+            }
+
+            try {
+                $event = $this->eventFactory->create($eventData);
+            } catch (IncompleteDataException $e) {
+                throw new ClientException(
+                    $requestSpecification->getName(),
+                    new IncompleteDataException($data, $dataIndex . '.' . $e->missingKey)
+                );
+            }
+
+            $events[] = $event;
+        }
+
+        return $events;
+    }
+}
