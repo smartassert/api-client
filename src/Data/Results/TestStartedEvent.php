b6dbdeb6ce761b8999d07e165a1d32069e2659d4
Jon Cram <webignition@gmail.com>
2026-06-24 16:17:49 +0100

    Model results test/started event (#410)

---
+++ src/Data/Results/TestStartedEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ApiClient\Data\Results;
+
+readonly class TestStartedEvent extends AbstractEncapsulatingEvent implements EventInterface
+{
+    public function getTest(): TestInterface
+    {
+        return new Test($this->getResourceReference());
+    }
+
+    /**
+     * @return StepInterface[]
+     */
+    public function getSteps(): array
+    {
+        $steps = [];
+
+        $relatedReferences = $this->getRelatedReferences() ?? [];
+
+        foreach ($relatedReferences as $relatedReference) {
+            $steps[] = new Step($relatedReference);
+        }
+
+        return $steps;
+    }
+
+    public function getConfiguration(): TestConfigurationInterface
+    {
+        $bodyData = $this->getBody();
+
+        $documentData = $bodyData['document'] ?? [];
+        $documentData = is_array($documentData) ? $documentData : [];
+
+        $payloadData = $documentData['payload'] ?? [];
+        $payloadData = is_array($payloadData) ? $payloadData : [];
+
+        $configurationData = $payloadData['config'] ?? [];
+        $configurationData = is_array($configurationData) ? $configurationData : [];
+
+        $browser = $configurationData['browser'] ?? '';
+        $browser = is_string($browser) ? $browser : '';
+
+        $url = $configurationData['url'] ?? '';
+        $url = is_string($url) ? $url : '';
+
+        return new TestConfiguration($browser, $url);
+    }
+}
